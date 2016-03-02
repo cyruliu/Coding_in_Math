8f6596ab3f898a8fae532de2858ca15482477292
donggobler <cyrusliu@bupt.edu.cn>
2016-03-02 15:25:21 +0800

    Solving equations

---
+++ Gauss列主元素消元法/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Gauss列主元素消元法
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataMatrix1 = new System.Windows.Forms.DataGridView();
+            this.btnSure = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRow = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataMatrix1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(36, 350);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(132, 29);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "确定计算求解";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 207);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "输入增广矩阵";
+            // 
+            // dataMatrix1
+            // 
+            this.dataMatrix1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataMatrix1.Location = new System.Drawing.Point(172, 115);
+            this.dataMatrix1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataMatrix1.Name = "dataMatrix1";
+            this.dataMatrix1.RowTemplate.Height = 23;
+            this.dataMatrix1.Size = new System.Drawing.Size(445, 204);
+            this.dataMatrix1.TabIndex = 11;
+            // 
+            // btnSure
+            // 
+            this.btnSure.Location = new System.Drawing.Point(381, 35);
+            this.btnSure.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSure.Name = "btnSure";
+            this.btnSure.Size = new System.Drawing.Size(73, 29);
+            this.btnSure.TabIndex = 14;
+            this.btnSure.Text = "确定";
+            this.btnSure.UseVisualStyleBackColor = true;
+            this.btnSure.Click += new System.EventHandler(this.btnSure_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 45);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "输入方程组个数";
+            // 
+            // txtRow
+            // 
+            this.txtRow.Location = new System.Drawing.Point(238, 35);
+            this.txtRow.Margin = new System.Windows.Forms.Padding(4);
+            this.txtRow.Name = "txtRow";
+            this.txtRow.Size = new System.Drawing.Size(112, 25);
+            this.txtRow.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(222, 357);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 15);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "方程组的解";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 442);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSure);
+            this.Controls.Add(this.dataMatrix1);
+            this.Controls.Add(this.txtRow);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOK);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataMatrix1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button btnOK;
+        public System.Windows.Forms.Label label2;
+        public System.Windows.Forms.DataGridView dataMatrix1;
+        public System.Windows.Forms.Button btnSure;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox txtRow;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Gauss列主元素消元法/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gauss列主元素消元法
+{
+    public partial class Form1 : Form
+    {        
+        public Form1()
+        {
+            InitializeComponent();
+        }                   
+        public void btnSure_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                int n;
+                n = int.Parse(txtRow.Text);
+                dataMatrix1.ColumnCount = n + 1;
+                dataMatrix1.RowCount = n;
+                for (int i = 0; i < n; i++)                
+                    dataMatrix1.Columns[i].Name = "x" + (i + 1).ToString();
+                dataMatrix1.Columns[n].Name = "b";                
+            }
+            catch(Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }         
+        }
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //赋值，将系数矩阵转存到数组里
+                int n;
+                n = int.Parse(txtRow.Text);
+                double[,] arrA = new double[n, n+1];
+                double[] arrB = new double[n];
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j <=n; j++)
+                        arrA[i, j] = double.Parse(dataMatrix1.Rows[i].Cells[j].Value.ToString());
+                    arrB[i] = double.Parse(dataMatrix1.Rows[i].Cells[n].Value.ToString());
+                }
+                string s = "";
+                if (Gauss(arrA, arrB, n) == 1)
+                {
+                    label3.Text = "此方法不能顺利进行！";
+                }
+                else
+                {
+                    for (int i = 0; i < n; i++)
+                        s += "x" + (i + 1).ToString() + "=" + arrB[i].ToString("f4") + " ";
+                    label3.Text = s;
+                }
+            }
+            catch(Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+        private int Gauss(double[,] arrA, double[] arrB, int n)
+        {
+            for (int k = 0; k < n-1; k++)
+            {
+                // 寻找第k列的主元素以及它所在的行号  
+                double t, mainElement;            // mainElement用于保存主元素的值       
+                int l;                            // 用于保存主元素所在的行号   
+                // 从第k行到第n行寻找第k列的主元素，记下主元素mainElement和所在的行号l  
+                mainElement = Math.Abs(arrA[k,k]);  // 取绝对值   
+                l = k;
+                for (int i = k + 1; i < n; i++)
+                {
+                    if (mainElement < Math.Abs(arrA[i,k]))
+                    {
+                        mainElement = Math.Abs(arrA[i,k]);
+                        l = i;                        // 记下主元素所在的行号    
+                    }
+                }
+                // l是主元素所在的行。将l行与k行交换，每行前面的k个元素都是0，不必交换        
+                if (l != k)
+                {
+                    for (int j = k; j <= n; j++)
+                    {
+                        t = arrA[k,j]; arrA[k,j] = arrA[l,j]; arrA[l,j] = t;
+                    }
+                }
+                //互换行后开始进行消元
+                for (int i = k + 1; i < n; i++)
+                {
+                    double d = arrA[i,k]/arrA[k,k];  // 使用变量d将每行的首元素a[i,k]的值保存下来。    
+                    for (int j = k; j <= n; j++) //开始消元
+                        arrA[i,j] = arrA[i,j] - d * arrA[k,j];
+                }
+            }
+            //k循环完后即可得整个消元矩阵a
+            //回代               
+
+                //回代               
+
+            for (int i =n-1; i >=0; i--)
+            {
+                for (int j = i + 1; j < n; j++)
+                    arrA[i,n] = arrA[i,n] - arrA[i, j] * arrB[j];
+                arrB[i] = arrA[i,n] / arrA[i, i];
+            }
+
+
+
+
+
+          //该算法一样，只是数组的存放方式不同。
+         //   int k, i, j, i0;
+         //   double element;
+         //   for (k = 0; k < n - 1; k++)
+         //   {
+         //       element = Math.Abs(arrA[k, k]);
+         //       i0 = k;
+         //       for ( i =k+1; i <n; i++)                
+         //           if (Math.Abs(arrA[i,k])>element)
+         //           {
+         //               element = Math.Abs(arrA[i, k]);
+         //               i0 = i;
+         //           }
+                
+         //       if (element < 0.00000001)
+         //           return 1;   
+         ////把主元素所在的行换到最上行
+         //       if (i0!=k)
+         //       {
+         //           for ( j = 0; j < n; j++)
+         //           {
+         //               element = arrA[k, j];
+         //               arrA[k, j] = arrA[i0, j];
+         //               arrA[i0, j] = element;
+         //           }
+         //           //互换常量两行
+         //           element = arrB[k];
+         //           arrB[k] = arrB[i0];
+         //           arrB[i0] = element;
+         //       }
+         //       //换完行后开始逐行消元化为上三角形
+         //       for (i = k + 1; i < n; i++)
+         //       {
+         //           double d = arrA[i, k] / arrA[k, k];
+         //           //arrA[i, k] = arrA[i, k] / arrA[k, k];
+         //           for (j = k; j < n; j++)
+         //               arrA[i, j] = arrA[i, j] - d* arrA[k, j];
+         //           arrB[i] = arrB[i] - d* arrB[k];
+         //       }
+         //   }
+         //   //回代               
+
+         //   for (i =n-1; i >=0; i--)
+         //   {
+         //       for (j = i + 1; j < n; j++)
+         //           arrB[i] = arrB[i] - arrA[i, j] * arrB[j];
+         //       arrB[i] = arrB[i] / arrA[i, i];
+         //   }
+
+           
+            return 0;
+        }
+    }
+}
