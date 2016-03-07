10a41295a7169ced2a168aa9804ee522ea86c19b
donggobler <cyrusliu@bupt.edu.cn>
2016-03-07 10:08:24 +0800

    LU decomposition for linear equations

---
+++ 直接三角分解法/LU分解/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace 直接三角分解法
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSure = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGrid1 = new System.Windows.Forms.DataGridView();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.dataGrid2 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(130, 41);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(72, 21);
+            this.txtNum.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "请输入方程组个数:\r\n";
+            // 
+            // btnSure
+            // 
+            this.btnSure.Location = new System.Drawing.Point(218, 39);
+            this.btnSure.Name = "btnSure";
+            this.btnSure.Size = new System.Drawing.Size(75, 23);
+            this.btnSure.TabIndex = 2;
+            this.btnSure.Text = "确定";
+            this.btnSure.UseVisualStyleBackColor = true;
+            this.btnSure.Click += new System.EventHandler(this.btnSure_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "请输入方程组系数增广矩阵:";
+            // 
+            // dataGrid1
+            // 
+            this.dataGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid1.Location = new System.Drawing.Point(130, 123);
+            this.dataGrid1.Name = "dataGrid1";
+            this.dataGrid1.RowTemplate.Height = 23;
+            this.dataGrid1.Size = new System.Drawing.Size(343, 192);
+            this.dataGrid1.TabIndex = 4;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(25, 342);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 30);
+            this.btnOK.TabIndex = 5;
+            this.btnOK.Text = "确定求解";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(130, 342);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(343, 30);
+            this.txtResult.TabIndex = 6;
+            // 
+            // dataGrid2
+            // 
+            this.dataGrid2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid2.Location = new System.Drawing.Point(526, 123);
+            this.dataGrid2.Name = "dataGrid2";
+            this.dataGrid2.RowTemplate.Height = 23;
+            this.dataGrid2.Size = new System.Drawing.Size(343, 192);
+            this.dataGrid2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(488, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "紧凑法变形后的LU矩阵:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 584);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGrid2);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.dataGrid1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSure);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNum);
+            this.Name = "Form1";
+            this.Text = "直接三角发求线性方程";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSure;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGrid1;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.DataGridView dataGrid2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ 直接三角分解法/LU分解/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 直接三角分解法
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSure_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n;
+                n = int.Parse(txtNum.Text);
+                dataGrid1.ColumnCount = n + 1;
+                dataGrid1.RowCount = n;
+                for (int i = 0; i < n; i++)
+                    dataGrid1.Columns[i].Name = "x" + (i + 1).ToString();
+                dataGrid1.Columns[n].Name = "b";
+                //变形后的矩阵存放地
+                dataGrid2.ColumnCount = n + 1;
+                dataGrid2.RowCount = n;
+                for (int i = 0; i < n; i++)
+                    dataGrid2.Columns[i].Name = "x" + (i + 1).ToString();
+                dataGrid2.Columns[n].Name = "b";
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }         
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //赋值，将系数矩阵转存到数组里
+                int n;
+                n = int.Parse(txtNum.Text);
+                double[,] arrA = new double[n, n + 1];
+                double[] arrB = new double[n];
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j <= n; j++)
+                        arrA[i, j] = double.Parse(dataGrid1.Rows[i].Cells[j].Value.ToString());                    
+                }
+                string s = "";
+                if (LU(arrA, arrB, n) == 1)
+                {
+                    txtResult.Text = "此方法不能顺利进行！";
+                }
+                else
+                {
+                    for (int i = 0; i < n; i++)
+                        s += "x" + (i + 1).ToString() + "=" + arrB[i].ToString("f4") + " ";
+                    txtResult.Text = s;
+                }
+                //将变形的矩阵输入到该区域dataGrid2中。
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j <= n; j++)
+                        dataGrid2.Rows[i].Cells[j].Value = double.Parse(arrA[i, j].ToString());
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+        private int LU(double[,] arrA, double[] arrB, int n)
+        {
+             //先求第一列
+            for (int i =1; i < n; i++)
+            {
+                arrA[i, 0] = arrA[i, 0] / arrA[0, 0];
+            }
+            //紧凑法对矩阵arrA进行变换。
+            for (int k = 1; k < n ; k++)
+            {
+                //求行U的值
+                for (int j = k; j <=n; j++)
+                {                    
+                    for(int i0=k-1;i0>=0;i0-- )
+                    arrA[k,j]=arrA[k,j]-arrA[k,i0]*arrA[i0,j];
+                }
+                //求列L的值
+                for (int i = k+1; i < n; i++)
+                {
+                    for (int i0 = k - 1; i0>=0; i0--)
+                        arrA[i, k] = arrA[i, k] - arrA[i, i0] * arrA[i0, k];
+                    arrA[i, k] = arrA[i, k] / arrA[k, k];                    
+                }
+
+            }
+              
+            //回代               
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                for (int j = i + 1; j < n; j++)
+                    arrA[i, n] = arrA[i, n] - arrA[i, j] * arrB[j];
+                if (Math.Abs(arrA[i,i])<0.00000001)                
+                    return 1;                
+                arrB[i] = arrA[i, n] / arrA[i, i];
+            }
+            return 0;
+        }
+        
+    }
+}
