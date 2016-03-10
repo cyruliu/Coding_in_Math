df4364c27163e4ea68a2e4d0385a1f8e481f1891
donggobler <cyrusliu@bupt.edu.cn>
2016-03-10 10:59:13 +0800

    Add Newton method for equations

---
+++ 非线性方程求解--Newton/Linear-equaltions-solving-Newton/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace 非线性方程求解__Newton
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dataGrid1 = new System.Windows.Forms.DataGridView();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtLimit = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMin = new System.Windows.Forms.TextBox();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.Location = new System.Drawing.Point(68, 25);
+            this.btnSolve.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(100, 29);
+            this.btnSolve.TabIndex = 2;
+            this.btnSolve.Text = "确定求解";
+            this.btnSolve.UseVisualStyleBackColor = true;
+            this.btnSolve.Click += new System.EventHandler(this.btnSolve_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(196, 25);
+            this.txtResult.Margin = new System.Windows.Forms.Padding(4);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(384, 29);
+            this.txtResult.TabIndex = 14;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.dataGrid1);
+            this.groupBox1.Controls.Add(this.txtNum);
+            this.groupBox1.Controls.Add(this.btnOk);
+            this.groupBox1.Location = new System.Drawing.Point(26, 17);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(823, 174);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "方程系数设定";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(76, 39);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(142, 15);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "非线性方程的次数：";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(70, 98);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(157, 15);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "请输入方程各项系数：";
+            // 
+            // dataGrid1
+            // 
+            this.dataGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid1.Location = new System.Drawing.Point(236, 86);
+            this.dataGrid1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGrid1.Name = "dataGrid1";
+            this.dataGrid1.RowTemplate.Height = 23;
+            this.dataGrid1.Size = new System.Drawing.Size(566, 81);
+            this.dataGrid1.TabIndex = 19;
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(236, 36);
+            this.txtNum.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(105, 25);
+            this.txtNum.TabIndex = 17;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(451, 33);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(100, 29);
+            this.btnOk.TabIndex = 16;
+            this.btnOk.Text = "设置";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click_1);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtLimit);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.txtMax);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtMin);
+            this.groupBox2.Controls.Add(this.txtStart);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(56, 200);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(750, 113);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "初始参数设定";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(331, 86);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 15);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "计算精度：";
+            // 
+            // txtLimit
+            // 
+            this.txtLimit.Location = new System.Drawing.Point(421, 75);
+            this.txtLimit.Margin = new System.Windows.Forms.Padding(4);
+            this.txtLimit.Name = "txtLimit";
+            this.txtLimit.Size = new System.Drawing.Size(100, 25);
+            this.txtLimit.TabIndex = 20;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(346, 30);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "最大值：";
+            // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(421, 23);
+            this.txtMax.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(100, 25);
+            this.txtMax.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(121, 33);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "最小值：";
+            // 
+            // txtMin
+            // 
+            this.txtMin.Location = new System.Drawing.Point(206, 27);
+            this.txtMin.Margin = new System.Windows.Forms.Padding(4);
+            this.txtMin.Name = "txtMin";
+            this.txtMin.Size = new System.Drawing.Size(105, 25);
+            this.txtMin.TabIndex = 16;
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(206, 78);
+            this.txtStart.Margin = new System.Windows.Forms.Padding(4);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(105, 25);
+            this.txtStart.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 78);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 15);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "请输入迭代初始值：";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnSolve);
+            this.groupBox3.Controls.Add(this.txtResult);
+            this.groupBox3.Location = new System.Drawing.Point(66, 332);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(615, 121);
+            this.groupBox3.TabIndex = 18;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "运算结果";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 500);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Newton迭代非线性方程求解";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSolve;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dataGrid1;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtLimit;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMin;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
